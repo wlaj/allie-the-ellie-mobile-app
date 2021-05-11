737de1738b19e7ea467d2af0c1751decf8b72b78
Lucas Jansen <wlajansen16@gmail.com>
2021-05-11 15:59:07 +0200

    v1.4.4 - Directory update, added animations, new tilset

--- Platform/PlayButton.gd
+++
@@ -1,7 +0,0 @@
-extends TextureButton
-
-
-
-
-func _on_PlayButton_pressed():
-	get_tree().change_scene("res://Level1.tscn")
---
+++ Platform/scenes/HUD.gd
@@ -0,0 +1,13 @@
+extends CanvasLayer
+
+var coins  = 0
+
+func _ready():
+	$Counter.text = String(coins)
+
+
+func _on_coin_collected():
+	 coins = coins + 1 
+	 _ready()
+	 if coins == 3:
+		 get_tree().change_scene("res://scenes/Menu.tscn")
