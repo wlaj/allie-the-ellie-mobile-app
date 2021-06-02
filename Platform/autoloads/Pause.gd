efaeaf024bbe343bf3b50985b98b9bae988f29f3
Lucas Jansen <wlajansen16@gmail.com>
2021-06-02 12:22:23 +0200

    v1.6.5 - Added minigames

--- Platform/autoloads/Pause.gd
+++ Platform/autoloads/Pause.gd
@@ -9,11 +9,15 @@ func _input(event):
 		set_visible(!get_tree().paused)
 		get_tree().paused = !get_tree().paused
 		
+<<<<<<< Updated upstream
 
 #func _on_Button_pressed():
 #	get_tree().paused = false
 #	set_visible(false)
 #
+=======
+	
+>>>>>>> Stashed changes
 func set_visible(is_visible):
 	for node in get_children():
 		node.visible = is_visible
@@ -28,7 +32,9 @@ func set_visible(is_visible):
 #		var master_sound = AudioServer.get_bus_index("Master")
 #		AudioServer.set_bus_mute(master_sound, false) 
 
+
 func _on_MuteButton_toggled(button_pressed):
+func _on_MusicButton_toggled(button_pressed):
 	if(button_pressed):
 		var master_sound = AudioServer.get_bus_index("Master")
 		AudioServer.set_bus_mute(master_sound, true)
@@ -44,3 +50,8 @@ func _on_PlayButton_pressed():
 
 func _on_HomeButton_pressed():
 	get_tree().reload_current_scene()
+
+
+func _on_PlayButton_pressed():
+	get_tree().paused = false
+	set_visible(false)
--- Platform/scenes/Allie.gd
+++ Platform/scenes/Allie.gd
@@ -11,11 +11,19 @@ func _physics_process(delta):
 	if Input.is_action_pressed("right"):
 		velocity.x = SPEED
 		$AnimationPlayer.play("Walk")
+<<<<<<< Updated upstream
 		$Spritesheet.flip_h = false
 	elif Input.is_action_pressed("left"):
 		velocity.x = -SPEED
 		$AnimationPlayer.play("Walk")
 		$Spritesheet.flip_h = true
+=======
+		$SpritesheetWalk.flip_h = false
+	elif Input.is_action_pressed("left"):
+		velocity.x = -SPEED
+		$AnimationPlayer.play("Walk")
+		$SpritesheetWalk.flip_h = true
+>>>>>>> Stashed changes
 	else:
 		$Sprite.play("idle")
 	if not is_on_floor():
