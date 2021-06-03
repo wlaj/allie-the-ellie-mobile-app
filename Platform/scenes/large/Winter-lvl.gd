224824de6716ea28beb33505ffe06ea6394c56bc
Lucas Jansen <wlajansen16@gmail.com>
2021-06-03 23:43:43 +0200

    v1.7.3 - Added snow, changed touch screen buttons

--- Platform/scenes/large/Winter-lvl.gd
+++ Platform/scenes/large/Winter-lvl.gd
@@ -7,3 +7,7 @@ func _ready():
 
 func _on_EndHouseButton_pressed():
 	get_tree().change_scene("res://scenes/small/Christmas-House-lvl.tscn")
+
+
+func _on_SnowflakeButton_pressed():
+	$Snow/CPUParticles2D.visible = true
